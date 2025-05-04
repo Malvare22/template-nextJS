b1dd07f2421f3e480e4fc8e116cc0c3dbd1b17c3
JulianQuirozG <75459275+JulianQuirozG@users.noreply.github.com>
2025-05-04 17:28:48 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: process.env.BASE_PATH,
-  assetPrefix: process.env.BASE_PATH,
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
 };
 
 export const getSrcImage = (s) => {
-  return `${process.env.BASE_PATH}/${s}`;
+  return `${process.env.NEXT_PUBLIC_BASE_PATH}/${s}`;
 };
 
 export default nextConfig;
